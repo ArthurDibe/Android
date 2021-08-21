0b30e4b012f97fd87a19e867ec65b0c364d0561b
Arthur Dibe <dibearthur@gmail.com>
2021-08-21 19:15:08 -0300

    KCalculator.kt: Added checkNumbers function to check if numbers are null or not

--- Kotlin/KCalculator.kt
+++ Kotlin/KCalculator.kt
@@ -2,3 +2,18 @@ fun main()
 {
     
 }
+
+// this function returns true if both numbers are NOT NULL
+// otherwise false
+fun checkNumbers(cn1:Float?, cn2:Float?=0f):Boolean
+{
+    var isNotNull = true
+    
+    if((cn1 == null) || (cn2 == null))
+    {
+        isNotNull = false
+        println("\tERROR ----> A NULL WAS FOUND!!!")
+    }
+    
+    return isNotNull
+}
