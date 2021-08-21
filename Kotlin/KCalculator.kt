b70ca01c46fe9109c4d27c222db41d14b61190cb
Arthur Dibe <dibearthur@gmail.com>
2021-08-21 19:24:28 -0300

    KCalculator.kt: Added subtract function to subtract two float numbers

--- Kotlin/KCalculator.kt
+++ Kotlin/KCalculator.kt
@@ -29,3 +29,15 @@ fun sum(sn1:Float?, sn2:Float?):Float?
     
 	return result
 }
+
+// Return the subtraction of 2 numbers if both numbers are not null
+// Return null otherwise
+fun subtract(sn1:Float?, sn2:Float?):Float?
+{
+    var result:Float? = null
+    
+   	if(checkNumbers(sn1, sn2))
+        result = (sn1?: -1f) - (sn2?: -1f)
+   
+	return result
+}
