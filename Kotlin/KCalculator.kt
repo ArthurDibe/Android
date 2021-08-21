af2f0960f17fe9416cf89e620779ffb5c5a3b5a6
Arthur Dibe <dibearthur@gmail.com>
2021-08-21 19:26:43 -0300

    KCalculator.kt: Added divide function to divide two float numbers

--- Kotlin/KCalculator.kt
+++ Kotlin/KCalculator.kt
@@ -54,3 +54,15 @@ fun multiply(sn1:Float?, sn2:Float?):Float?
     
 	return result
 }
+
+// Return the division of 2 numbers if both numbers are not null
+// Return null otherwise
+fun divide(sn1:Float?, sn2:Float?):Float?
+{
+    var result:Float? = null
+    
+   	if(checkNumbers(sn1, sn2)) 
+    	result = (sn1?: -1f) / (sn2?: -1f)
+    
+	return result
+}
