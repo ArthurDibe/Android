4f70328231b6c21d171550347e83c916de788de4
Arthur Dibe <dibearthur@gmail.com>
2021-08-27 13:05:41 -0300

    MainActivity.kt: Added logic for add clicks and remove clicks

--- DebuggingTest/app/src/main/java/me/dibe/debuggingtest/MainActivity.kt
+++ DebuggingTest/app/src/main/java/me/dibe/debuggingtest/MainActivity.kt
@@ -1,11 +1,29 @@
 package me.dibe.debuggingtest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lblDebug = lblDebug
+        val btnClick = btnClicked
+        val btnUnclick = btnUnclicked
+        var count: Int = 0
+
+        btnClick.setOnClickListener {
+            lblDebug.setTextColor(Color.GREEN)
+            lblDebug.setText((++count).toString())
+        }
+
+        btnUnclick.setOnClickListener {
+            lblDebug.setTextColor(Color.RED)
+            lblDebug.setText((--count).toString())
+        }
     }
 }
