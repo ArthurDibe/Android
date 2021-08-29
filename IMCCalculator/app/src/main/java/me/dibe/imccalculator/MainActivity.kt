e83fc6bbfa8c6f563103048460031c8ef945c5ee
Arthur Dibe <dibearthur@gmail.com>
2021-08-29 16:31:01 -0300

    MainActivity.kt: Created IMC layout for allowing user to enter weight and height

--- IMCCalculator/app/src/main/java/me/dibe/imccalculator/MainActivity.kt
+++ IMCCalculator/app/src/main/java/me/dibe/imccalculator/MainActivity.kt
@@ -3,12 +3,47 @@ package me.dibe.imccalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    var height_:String = ""
+    var weight_:String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.w("LIFE CYCLE", "CREATE - Screen is being created (no components yet) !")
+        setListeners()
+    }
+
+    private fun setListeners()
+    {
+        input_height?.doOnTextChanged { text, _, _, _ ->
+            height_ = text.toString()
+        }
+
+        input_weight?.doOnTextChanged { text, _, _, _ ->
+            weight_ = text.toString()
+        }
+
+        button?.setOnClickListener {
+            calculateIMC(height_, weight_)
+        }
+    }
+
+    private fun calculateIMC(pheight: String, pweight: String)
+    {
+        val lheight = pheight.toFloatOrNull()
+        val lweight = pweight.toFloatOrNull()
+
+        if(lheight != null && lweight != null){
+            var resultIMC = (lweight / (lheight * lheight))
+
+            result.text = "Your IMC ${String.format("%.2f", resultIMC)}"
+        }
     }
 
     override fun onStart(){
