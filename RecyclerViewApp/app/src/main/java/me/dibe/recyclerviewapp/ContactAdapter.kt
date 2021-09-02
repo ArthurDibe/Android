28e607936504f37e263d9c2324b96f75a11ca50f
Arthur Dibe <dibearthur@gmail.com>
2021-09-02 07:11:12 -0300

    ContactAdapter.kt: Implemented onCreateViewHolder function in order to create a new ViewHolder on screen every time that data is being changed or included new data

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
@@ -1,5 +1,6 @@
 package me.dibe.recyclerviewapp
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -7,9 +8,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter(private val contactList: List<Contact>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
-    
+
+    // This method is called by the RecyclerView when it is time to create a new ViewHolder on screen
+    //      parent parameter -> needed to access RecyclerView and Activity layout
+    //      viewType parameter -> only needed when we need to use different types of row layout in the same RecyclerView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.single_contact, parent, false) // context -> is the Activity
+
+        return ContactViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
