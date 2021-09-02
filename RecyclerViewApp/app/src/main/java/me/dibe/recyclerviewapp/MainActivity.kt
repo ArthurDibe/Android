f193b21a98ce71e33ccad8572e87b12a5f1efcfe
Arthur Dibe <dibearthur@gmail.com>
2021-09-02 07:16:07 -0300

    MainActivity.kt: Created generateDummyList function

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/MainActivity.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/MainActivity.kt
@@ -11,4 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    // This function generates dummy data, the user needs to pass the number of rows to be generated
+    private fun generateDummyList(size: Int): List<Contact> {
+        val list = ArrayList<Contact>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_meditate
+                else -> R.drawable.ic_smile
+            }
+            val item = Contact(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
+
 }
