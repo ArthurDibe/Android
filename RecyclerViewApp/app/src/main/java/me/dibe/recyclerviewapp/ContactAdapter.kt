11f11f9ece7173a2eec088d89c9606539c0d7873
Arthur Dibe <dibearthur@gmail.com>
2021-09-02 07:09:37 -0300

    ContactAdapter.kt: Added empty basic functions that are important to insert data from each ViewHolders into each one of the rows in the RecyclerView

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
@@ -1,11 +1,24 @@
 package me.dibe.recyclerviewapp
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+class ContactAdapter(private val contactList: List<Contact>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
     class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val contactImage: ImageView = itemView.findViewById(R.id.contact_image) // itemView.contact_image
