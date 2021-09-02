84a67ffd3da4780808e968162e65a144ac8072f3
Arthur Dibe <dibearthur@gmail.com>
2021-09-02 19:20:43 -0300

    Created COntactsApplication class and made it to inherit from Applpication class

---
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactsApplication.kt
@@ -0,0 +1,11 @@
+package me.dibe.recyclerviewapp
+
+import android.app.Application
+
+class ContactsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
