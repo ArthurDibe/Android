f98d22eee04f41052db4a238d232d2b5581a2239
Arthur Dibe <dibearthur@gmail.com>
2021-09-02 07:08:13 -0300

    ContactAdapter.kt: Created inner class ContactViewHolder to hold the information about each row contact

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/ContactAdapter.kt
@@ -1,5 +1,15 @@
 package me.dibe.recyclerviewapp
 
-class ContactAdapter {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+
+    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val contactImage: ImageView = itemView.findViewById(R.id.contact_image) // itemView.contact_image
+        val contactName: TextView = itemView.findViewById(R.id.contact_name)    // itemView.contact_name
+        val contactPhone: TextView = itemView.findViewById(R.id.contact_phone)  // itemView.contact_phone
+    }
 }
