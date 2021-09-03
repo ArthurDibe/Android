f560900141b3dd875efec34a5b232135e59f343e
Arthur Dibe <dibearthur@gmail.com>
2021-09-03 03:02:33 -0300

    MainActivity.kt: Added function to hold handlers

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/MainActivity.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/MainActivity.kt
@@ -2,11 +2,12 @@ package me.dibe.recyclerviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         recycler_view.adapter = ContactAdapter(contactExampleList)
         recycler_view.layoutManager = LinearLayoutManager(this) // layoutManager is responsible to positioning the items in the recyclerView
         recycler_view.setHasFixedSize(true) // This is a performance optimization
+        setListeners()
+    }
+
+    private fun setListeners()
+    {
+        btn_search?.setOnClickListener {
+            Log.e("SEARCH CLICKED", "Button search was clicked!!!")
+        }
     }
 
     // This function generates dummy data, the user needs to pass the number of rows to be generated
@@ -28,7 +37,7 @@ class MainActivity : AppCompatActivity() {
                 1 -> R.drawable.ic_meditate
                 else -> R.drawable.ic_smile
             }
-            val item = Contact(drawable, "Item $i", "Line 2")
+            val item = Contact(drawable, i,"FName $i", " LName",i+1, "0000000000")
             list += item
         }
         return list
