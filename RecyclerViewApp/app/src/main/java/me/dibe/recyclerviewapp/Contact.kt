1b1e5eefc73a0bae08d209a901055181d496014e
Arthur Dibe <dibearthur@gmail.com>
2021-09-03 03:07:22 -0300

    Contact.kt: added id, fname, lname, age

--- RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/Contact.kt
+++ RecyclerViewApp/app/src/main/java/me/dibe/recyclerviewapp/Contact.kt
@@ -2,6 +2,9 @@ package me.dibe.recyclerviewapp
 
 data class Contact (
     val imageResource: Int,
-    val name: String,
+    val id: Int,
+    val fname: String,
+    val lname: String,
+    val age: Int,
     val phone: String
 )
